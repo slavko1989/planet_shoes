43df17a95bd1932b998caff70dbf58f28346d01b
slavko1989 <slavkoslave1989@gmail.com>
2023-06-05 06:14:23 +0200

    components for user panel

--- spl_autoload_class/security.php
+++ spl_autoload_class/security.php
@@ -1,4 +1,101 @@
 <?php
+
+function user_info(){
+	include_once(__DIR__."../../spl_autoload_class/autoload_class.php");
+$interface = new Interface_admin();
+$user_panel = new Interface_class();
+
+$interface->head();
+if(isset($_SESSION["user_email"]) && isset($_SESSION["user_id"])){
+$interface->top_c();
+$user_panel->user_sidebar();
+$user_panel->user_dashboard();
+$count = new Prd_View();
+$view = new UserView();
+$view->add_user_info();
+
+    $account = $view->return_user_info();
+    include_once(__DIR__."../../components/user_info_component.php");
+$interface->footer();
+}else{
+	echo "you don't have permission";
+}
+}
+
+
+function user_payment(){
+	include_once(__DIR__."../../spl_autoload_class/autoload_class.php");
+$interface = new Interface_class();
+$user_panel = new Interface_admin();
+$interface->head();
+if(isset($_SESSION["user_email"]) && isset($_SESSION["user_id"])){
+$interface->sidebar();
+$interface->top_menu();
+$sold = new Sold_View();
+$info_user = new UserView();
+$sold_id = $sold->view_all_sold_product();
+$info = $info_user->return_user_info();
+$cart = new Shop_View();
+$cart->get_delete_shopID();
+$cart->get_update_q();
+
+include_once(__DIR__."../../components/user_payment_component.php");
+$interface->footer();
+}else{
+	echo "you don't have permission";
+}
+}
+
+
+function user_bought(){
+
+include_once(__DIR__."../../spl_autoload_class/autoload_class.php");
+
+
+$interface = new Interface_admin();
+$user_panel = new Interface_class();
+$sold = new Sold_View();
+$interface->head();
+if(isset($_SESSION["user_email"]) && isset($_SESSION["user_id"])){
+$interface->top_c();
+$user_panel->user_sidebar();
+$user_panel->user_dashboard();
+$sold->delete_from_sold();
+$id_sold = $sold->view_all_sold_product();
+include_once(__DIR__."../../components/user_bought_component.php");
+$interface->footer();
+}else{
+	echo "you don't have permission";
+}
+
+}
+
+
+function user_pass(){
+
+	include_once(__DIR__."../../spl_autoload_class/autoload_class.php");
+$interface = new Interface_admin();
+$user_panel = new Interface_class();
+
+$interface->head();
+if(isset($_SESSION["user_email"]) && isset($_SESSION["user_id"])){
+$interface->top_c();
+$user_panel->user_sidebar();
+$user_panel->user_dashboard();
+$count = new Prd_View();
+
+	$view = new UserView();
+    $view->delete_user_account();
+    $view->update_pass();
+    $view->delete_user();
+    $account = $view->show_user_account();
+include_once(__DIR__."../../components/user_change_pass_component.php");
+$interface->footer();
+}else{
+	echo "you don't have permission";
+} }
+
+
 function admin_index(){
 include_once(__DIR__."../../spl_autoload_class/autoload_class.php");
 $interface = new Interface_admin();
