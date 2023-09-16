da182c66d321e17e49c2c8f129c52fe9911462d1
slavko1989 <118732826+slavko1989@users.noreply.github.com>
2023-09-16 18:03:06 +0200

    Update brand_edit.php

--- brand/brand_edit.php
+++ brand/brand_edit.php
@@ -2,7 +2,7 @@
 
 include_once(__DIR__."../../spl_autoload_class/security.php");
 
-brand_edit();
+brand_edit();// skripta u admin panelu za editovanje brendova, sve o ovoj funkciji je objasnjeno u skripti
 
 ?>
  
