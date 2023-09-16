8c2897bebd276816b46a043fea35b27f03b3858e
slavko1989 <118732826+slavko1989@users.noreply.github.com>
2023-09-16 17:49:08 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once(__DIR__."../../spl_autoload_class/security.php");
-admin_index();
+admin_index(); //funkcija koja vraca pocetnu stranicu admin panela. Objasnjenje u toj skiprti
 
 ?>
 
